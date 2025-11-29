f0a62de714eac214c5ff188d2706b214ff8621d8
waterwu <boatwind@gmail.com>
2025-11-29 11:48:48 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
     },
   },
   server: {
+    port: 5178,
     proxy: {
       '/api': {
         target: 'http://localhost:5001', changeOrigin: true, secure: false,
