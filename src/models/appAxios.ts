3cab7e0156fd2319d0339ede1b5dada5fb61bd02
waterwu <boatwind@gmail.com>
2025-12-01 09:25:26 +0800

    feat: 添加 cookie-parser 依赖并重构 axios 请求拦截器
    添加 cookie-parser 及其类型定义以支持服务端 cookie 处理
    重构 axios 实例，使用拦截器统一管理请求头，包括添加 Authorization 头

--- src/models/appAxios.ts
+++ src/models/appAxios.ts
@@ -5,8 +5,27 @@ import axios from 'axios';
 
 export const appAxios = axios.create({
   baseURL: '/api',
-  headers: {
-    'Content-Type': 'application/json',
-    'x-locale': store._get('locale'),
-  },
 });
+
+// 添加请求拦截器，统一管理请求头
+appAxios.interceptors.request.use(
+  (config) => {
+    // 设置基础请求头
+    config.headers['Content-Type'] = 'application/json';
+    config.headers['x-locale'] = store._get('locale');
+    
+    // 从 store 获取 access_token
+    const accessToken = store._get('session.access_token');
+    
+    // 如果存在 access_token，则添加到 Authorization 头
+    if (accessToken) {
+      config.headers['Authorization'] = `Bearer ${accessToken}`;
+    }
+    
+    return config;
+  },
+  (error) => {
+    // 处理请求错误
+    return Promise.reject(error);
+  }
+);
